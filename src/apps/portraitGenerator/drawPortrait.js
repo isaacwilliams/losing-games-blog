3f6f3c4965a6740af174611d9d4becc494b90fa1
Isaac Williams <isaac@isaacwilliams.net>
2018-11-27 14:28:00 +1100

    Portrait generator: more bodies, facial hair

--- src/apps/portraitGenerator/drawPortrait.js
+++ src/apps/portraitGenerator/drawPortrait.js
@@ -15,6 +15,7 @@ const drawPortrait = async (canvas, skinVariance = 12, hairVariance = 24, clothi
         imgMouths,
         imgHairFront,
         imgHairBack,
+        imgHairFace,
         imgBodies,
         imgBodiesSkin,
         imgAccessoryFront,
@@ -26,6 +27,7 @@ const drawPortrait = async (canvas, skinVariance = 12, hairVariance = 24, clothi
         require('./img/mouths.png'),
         require('./img/hair-front.png'),
         require('./img/hair-back.png'),
+        require('./img/hair-face.png'),
         require('./img/bodies.png'),
         require('./img/bodies-skin.png'),
         require('./img/accessory-front.png'),
@@ -46,13 +48,14 @@ const drawPortrait = async (canvas, skinVariance = 12, hairVariance = 24, clothi
     const mouths = createSpriteSheet(imgMouths, 512, 512);
     const hairFront = createSpriteSheet(imgHairFront, 512, 512);
     const hairBack = createSpriteSheet(imgHairBack, 512, 512);
+    const hairFace = createSpriteSheet(imgHairFace, 512, 512);
     const bodies = createSpriteSheet(imgBodies, 512, 512);
     const bodiesSkin = createSpriteSheet(imgBodiesSkin, 512, 512);
     const accessoryFront = createSpriteSheet(imgAccessoryFront, 512, 512);
     const accessoryBack = createSpriteSheet(imgAccessoryBack, 512, 512);
 
     const hairId = r(hairFront.size);
-    const bodyId = r(12);
+    const bodyId = r(bodies.size);
     const accessoryId = r(accessoryFront.size);
 
     ctx.drawImage(recolorImage(accessoryBack.sprite(accessoryId), accessoryColor), 0, 0);
@@ -62,6 +65,7 @@ const drawPortrait = async (canvas, skinVariance = 12, hairVariance = 24, clothi
     ctx.drawImage(recolorImage(heads.sprite(r(heads.size)), skinColor), 0, 0);
     ctx.drawImage(mouths.sprite(r(mouths.size)), 0, 0);
     ctx.drawImage(eyes.sprite(r(eyes.size)), 0, 0);
+    Math.random() < 0.1 && ctx.drawImage(recolorImage(hairFace.sprite(r(4)), hairColor), 0, 0);
     ctx.drawImage(recolorImage(noses.sprite(r(noses.size)), skinColor), 0, 0);
     ctx.drawImage(recolorImage(hairFront.sprite(hairId), hairColor), 0, 0);
     ctx.drawImage(recolorImage(accessoryFront.sprite(accessoryId), accessoryColor), 0, 0);
