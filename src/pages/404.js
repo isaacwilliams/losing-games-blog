35cced8128bf19f1f9c41b33a3692f204ea0a4dd
Isaac Williams <isaac@isaacwilliams.net>
2018-11-28 13:57:26 +1100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 import PageLayout from '../components/layout/PageLayout'
+import PageTitle from '../components/page/PageTitle';
+import RichContent from '../components/shared/RichContent';
+
+const CenterAlign = styled.p`
+    margin-top: 2rem;
+    text-align: center;
+`
+
+const ErrorWrapper = styled.div`
+    padding: 6rem 0;
+`
+
+const ErrorTitle = styled(PageTitle)`
+    &:before,
+    &:after {
+        content: '✕ ✕ ✕';
+    }
+`
 
 const NotFoundPage = () => (
     <PageLayout>
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <ErrorWrapper>
+            <ErrorTitle>
+                Page not found
+            </ErrorTitle>
+            <CenterAlign>Please <Link to="/">return home</Link> before something even worse happens.</CenterAlign>
+        </ErrorWrapper>
     </PageLayout>
 )
 
