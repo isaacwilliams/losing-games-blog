509d8ce797be9a77133d384f7187813ad76690a9
Isaac Williams <isaac@isaacwilliams.net>
2018-11-29 11:20:12 +1100

    Fix width of media query

--- src/components/styles/media.js
+++ src/components/styles/media.js
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 const sizes = {
     desktop: 992,
     tablet: 768,
-    phone: 576,
+    phone: 672,
 }
 
 // Iterate through the sizes and create a media template
