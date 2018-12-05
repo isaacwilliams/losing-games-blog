9e3720a19ac98ea294f741feacd77eac8f9b5bee
Isaac Williams <isaac@isaacwilliams.net>
2018-12-05 15:23:57 +1100

    Updates to sharing cards

--- src/components/layout/SiteContainer.js
+++ src/components/layout/SiteContainer.js
@@ -45,7 +45,9 @@ const SiteContainer = ({ children }) => (
             <>
                 <Helmet title={data.site.siteMetadata.title}
                         meta={[
-                          { name: 'description', content: 'A blog about D&D & other games' },
+                            { name: 'description', content: 'A blog about D&D and other games' },
+                            { name: 'og:description', content: 'A blog about D&D and other games' },
+                            { name: 'og:title', content: data.site.siteMetadata.title },
                         ]}>
                     <html lang="en" />
                     <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
