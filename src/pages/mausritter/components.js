c9cbc4bea0148eb3c746a3af6e452acef4dfd4f7
Isaac Williams <isaac@isaacwilliams.net>
2019-08-19 11:46:33 +1000

    Add mouse generator page

---
+++ src/pages/mausritter/components.js
@@ -0,0 +1,177 @@
+import styled, { css } from 'styled-components';
+
+import PageLayout from '../../components/layout/PageLayout';
+import RichContent from '../../components/shared/RichContent';
+import PageTitle from '../../components/page/PageTitle';
+import PageHeader from '../../components/page/PageHeader';
+
+export const fontBroken = css`
+    font-family: ff-brokenscript-bc-web, Vollkorn, serif;
+    font-weight: 700;
+    letter-spacing: 0.03em;
+`;
+
+export const MausritterPageHeader = styled(PageHeader)`
+    padding-top: 4rem;
+    padding-bottom: 5rem;
+    color: white;
+
+    background: url(${require('./pencil-background.png')});
+    background-size: 400px auto;
+`
+
+export const MausritterPageTitle = styled(PageTitle)`
+    ${fontBroken}
+
+    font-size: 5rem;
+    line-height: 1em;
+
+    &:before,
+    &:after {
+        content: '';
+    }
+`
+
+export const MausritterTagline = styled.p`
+    ${fontBroken}
+
+    font-size: 1.65rem;
+    text-align: center;
+
+    &:before,
+    &:after {
+        content: '';
+    }
+`
+
+export const H2 = styled.h2`
+    ${fontBroken}
+    margin-bottom: 0.2rem;
+    font-size: 1.65rem;
+`
+
+export const H3 = styled.h3`
+    font-weight: 600;
+    margin-top: 0.5rem;
+    margin-bottom: 0.1rem;
+`
+
+export const Image = styled.img`
+    width: 100%;
+    height: auto;
+`;
+
+export const ContentContainer = styled.div`
+    margin: 2rem auto;
+    max-width: 60rem;
+`
+
+export const ResourceList = styled.ul`
+    li {
+        margin-left: 1em;
+        list-style: disc;
+    }
+
+    a {
+        color: black;
+
+        &:hover {
+            background: yellow;
+        }
+    }
+`
+
+export const ColumnContainer = styled.div`
+    padding: 1em 0;
+    display: flex;
+`;
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    flex-basis: calc(50% - 1rem);
+    padding-right: 0.5rem;
+
+    &.center-align {
+        justify-content: center;
+    }
+
+    &:last-child {
+        padding-right: 0;
+        padding-left: 0.5rem;
+    }
+`
+
+export const Column3 = styled(Column)`
+    flex-basis: calc(33.33% - 1rem);
+`
+
+export const BuySection = styled.div`
+    margin-top: 3rem;
+`
+
+export const DownloadSection = styled(ColumnContainer)`
+    font-size: 0.9em;
+
+    h4 {
+        margin-bottom: 0.5em;
+        font-weight: 600;
+    }
+
+    p {
+        margin-bottom: 0.5em;
+    }
+`;
+
+export const DownloadButton = styled.a`
+    display: inline-block;
+    padding: 0.1em 0.8em 0.2em 0.8em;
+
+    border-radius: 3px;
+
+    text-decoration: none;
+    font-weight: 600;
+
+    background: black;
+    color: white;
+
+    &:hover {
+        background: black;
+        color: white;
+    }
+`;
+
+
+export const BuyButton = styled.a`
+    position: relative;
+    display: block;
+
+    padding: 1em 1em;
+
+    border-radius: 3px;
+    box-shadow: 0 2px 0 black;
+
+    text-align: center;
+    text-decoration: none;
+    font-weight: 600;
+
+    background: yellow;
+    color: black;
+
+    &:hover {
+        top: -1px;
+        box-shadow: 0 3px 0 black;
+    }
+
+    &:active {
+        top: 1px;
+        box-shadow: 0 1px 0 black;
+    }
+`
+
+export const BuyButtonDescription = styled.span`
+    display: block;
+    font-size: 0.8em;
+    font-weight: normal;
+`
--- src/pages/mausritter/index.js
+++ src/pages/mausritter/index.js
@@ -4,183 +4,31 @@ import styled, { css } from 'styled-components';
 
 import * as colors from '../../components/styles/colors';
 import { fontDisplay } from '../../components/styles/fonts';
+
 import PageLayout from '../../components/layout/PageLayout';
 import RichContent from '../../components/shared/RichContent';
 import PageTitle from '../../components/page/PageTitle';
 import PageHeader from '../../components/page/PageHeader';
-
 import MausritterCharacter from '../../apps/mausritterCharacter/MausritterCharacter';
 
-const fontBroken = css`
-    font-family: ff-brokenscript-bc-web, Vollkorn, serif;
-    font-weight: 700;
-    letter-spacing: 0.03em;
-`;
-
-const MausritterPageHeader = styled(PageHeader)`
-    padding-top: 4rem;
-    padding-bottom: 5rem;
-    color: white;
-
-    background: url(${require('./pencil-background.png')});
-    background-size: 400px auto;
-`
-
-const MausritterPageTitle = styled(PageTitle)`
-    ${fontBroken}
-
-    font-size: 5rem;
-    line-height: 1em;
-
-    &:before,
-    &:after {
-        content: '';
-    }
-`
-
-const MausritterTagline = styled.p`
-    ${fontBroken}
-
-    font-size: 1.65rem;
-    text-align: center;
-
-    &:before,
-    &:after {
-        content: '';
-    }
-`
-
-const H2 = styled.h2`
-    ${fontBroken}
-    margin-bottom: 0.2rem;
-    font-size: 1.65rem;
-`
-
-const H3 = styled.h3`
-    font-weight: 600;
-    margin-top: 0.5rem;
-    margin-bottom: 0.1rem;
-`
-
-const Image = styled.img`
-    width: 100%;
-    height: auto;
-`;
-
-const ContentContainer = styled.div`
-    margin: 2rem auto;
-    max-width: 60rem;
-`
-
-const ResourceList = styled.ul`
-    li {
-        margin-left: 1em;
-        list-style: disc;
-    }
-
-    a {
-        color: black;
-
-        &:hover {
-            background: yellow;
-        }
-    }
-`
-
-const ColumnContainer = styled.div`
-    padding: 1em 0;
-    display: flex;
-`;
-
-const Column = styled.div`
-    display: flex;
-    flex-direction: column;
-
-    flex-basis: calc(50% - 1rem);
-    padding-right: 0.5rem;
-
-    &.center-align {
-        justify-content: center;
-    }
-
-    &:last-child {
-        padding-right: 0;
-        padding-left: 0.5rem;
-    }
-`
-
-const Column3 = styled(Column)`
-    flex-basis: calc(33.33% - 1rem);
-`
-
-const BuySection = styled.div`
-    margin-top: 3rem;
-`
-
-const DownloadSection = styled(ColumnContainer)`
-    font-size: 0.9em;
-
-    h4 {
-        margin-bottom: 0.5em;
-        font-weight: 600;
-    }
-
-    p {
-        margin-bottom: 0.5em;
-    }
-`;
-
-const DownloadButton = styled.a`
-    display: inline-block;
-    padding: 0.1em 0.8em 0.2em 0.8em;
-
-    border-radius: 3px;
-
-    text-decoration: none;
-    font-weight: 600;
-
-    background: black;
-    color: white;
-
-    &:hover {
-        background: black;
-        color: white;
-    }
-`;
-
-
-const BuyButton = styled.a`
-    position: relative;
-    display: block;
-
-    padding: 1em 1em;
-
-    border-radius: 3px;
-    box-shadow: 0 2px 0 black;
-
-    text-align: center;
-    text-decoration: none;
-    font-weight: 600;
-
-    background: yellow;
-    color: black;
-
-    &:hover {
-        top: -1px;
-        box-shadow: 0 3px 0 black;
-    }
-
-    &:active {
-        top: 1px;
-        box-shadow: 0 1px 0 black;
-    }
-`
-
-const BuyButtonDescription = styled.span`
-    display: block;
-    font-size: 0.8em;
-    font-weight: normal;
-`
+import {
+    MausritterPageHeader,
+    MausritterPageTitle,
+    MausritterTagline,
+    H2,
+    H3,
+    Image,
+    ContentContainer,
+    ResourceList,
+    ColumnContainer,
+    Column,
+    Column3,
+    BuySection,
+    DownloadSection,
+    DownloadButton,
+    BuyButton,
+    BuyButtonDescription,
+} from './components';
 
 const MausritterIndex = (props) => (
     <PageLayout fullWidthContent>
@@ -268,7 +116,7 @@ const MausritterIndex = (props) => (
                             Get playing right away with the instant mouse generator.
                         </p>
                         <p>
-                            <DownloadButton href="/tools/mausritter-character">Create a mouse</DownloadButton>
+                            <DownloadButton href="/mausritter/mouse">Create a mouse</DownloadButton>
                         </p>
                     </RichContent>
 
@@ -334,9 +182,9 @@ const MausritterIndex = (props) => (
                 <Column3>
                     <H2>GM Resources</H2>
 
-                    <RichContent>
-                        <p>Content generators</p>
-                    </RichContent>
+                    <H3>
+                        Sandbox content generators
+                    </H3>
 
                     <ResourceList>
                         <li><a href="../2019-07-24-mausritter-locations">Locations</a></li>
