40327009b5a74e3e1175ad9ac2be5bb40642aefb
Isaac Williams <isaac@isaacwilliams.net>
2022-02-04 16:26:28 +1100

    Updates

---
+++ dicematch/precache-manifest.a4dfc07e541f34b57709e6dfd93e1993.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0243f4c61ea85a181fd3",
+    "url": "/dicematch/static/css/main.f2eea392.chunk.css"
+  },
+  {
+    "revision": "0243f4c61ea85a181fd3",
+    "url": "/dicematch/static/js/main.63be93ec.chunk.js"
+  },
+  {
+    "revision": "ab9b5f51ac2d4afdfe24",
+    "url": "/dicematch/static/js/runtime~main.979d6832.js"
+  },
+  {
+    "revision": "682fabb2af67a01b48af",
+    "url": "/dicematch/static/js/2.68d6b1d6.chunk.js"
+  },
+  {
+    "revision": "c70881f6290e7023f6a35a29a3af9d0c",
+    "url": "/dicematch/static/media/icon-help.c70881f6.svg"
+  },
+  {
+    "revision": "9693a6fc478d8e8d4ef8cc2b2725fc41",
+    "url": "/dicematch/static/media/icon-settings.9693a6fc.svg"
+  },
+  {
+    "revision": "715a1ddf96ade32a8d8eaf3cb41c2bdc",
+    "url": "/dicematch/static/media/dicematch-logo-wide.715a1ddf.svg"
+  },
+  {
+    "revision": "16c320ea50252b588ae12824ee40112f",
+    "url": "/dicematch/index.html"
+  }
+];
